e35a77ce1b66c931fe981cd5de0b7b5a251c3327
JS2L <84076925+JS2L@users.noreply.github.com>
2021-06-09 17:47:27 +0900

    초기 커밋.

---
+++ 머신러닝 주차예측/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using 머신러닝_주차예측ML.Model;
+namespace 머신러닝_주차예측
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add input data
+            var input = new ModelInput()
+            {
+                SystemCodeNumber = "BHMBCCMKT01",
+                Capacity = 577,
+                LastUpdated = "2016-10-05 12:29"
+            };
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+            Console.WriteLine("예측점유율{0}:", result.Score);
+        }
+    }
+}
